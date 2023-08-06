593065b69a12df7b60349bd0ffaf67c29f1235ee
Gaming Gaming <102022231+Gaming987@users.noreply.github.com>
2023-08-06 20:10:06 +0530

    Just chill

---
+++ config.py
@@ -0,0 +1,2 @@
+MAIN_COLOR = 0xFF1B1B  #Main colour for bot's embed
+RED_COLOR = 0xFF0000
---
+++ restart.py
@@ -0,0 +1,7 @@
+import time
+import os
+
+time.sleep(30)
+os.system("kill 1")
+time.sleep(5)
+os.system("python main.py")
